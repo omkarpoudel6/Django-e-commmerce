c229e39b20caa491cc7d68e8fb1f754c907a3371
omkarpoudel6 <omkar.poudel77@gmail.com>
2020-09-03 04:56:36 -0700

    updating user profile done

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from user.models import UserProfile
 
 class SignUpForm(UserCreationForm):
     username=forms.CharField(max_length=30,label="User Name")
@@ -11,3 +12,33 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model=User
         fields=('username','email','first_name','last_name','password1','password2',)
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model=User
+        fields=('username','email','first_name','last_name',)
+        widgets={
+            'username':forms.TextInput(attrs={'class':'form-control','placeholder':'username'}),
+            'email':forms.EmailInput(attrs={'class':'form-control','placeholder':'email'}),
+            'first_name':forms.TextInput(attrs={'class':'form-control','placeholder':'first_name'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control','placeholder':'last_name'})
+        }
+
+CITY=[
+    ('Itahari','Itahari'),
+    ('Dharan','Dharan'),
+    ('Kathmandu','Kathmandu'),
+    ('Pokhara','Pokhara')
+]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        fields=('phone','address','city','country','image',)
+        widgets={
+            'phone':forms.TextInput(attrs={'class':'form-control','placeholder':'phone'}),
+            'address':forms.TextInput(attrs={'class':'form-control','placeholder':'address'}),
+            'city':forms.Select(attrs={'class':'form-control','placholder':'city'},choices=CITY),
+            'country':forms.TextInput(attrs={'class':'form-control','placeholder':'country'}),
+            'image':forms.FileInput(attrs={'class':'form-control','placeholder':'image'})
+        }
--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from user.views import *
 
 urlpatterns=[
-    path("",profile,name='profile')
+    path("",profile,name='profile'),
+    path("update/",update_user,name='update_user')
 ]
