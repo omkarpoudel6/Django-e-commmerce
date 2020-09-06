cae132a435cb33239fad524103cb9dac58f46b97
omkarpoudel6 <omkar.poudel77@gmail.com>
2020-09-05 21:03:36 -0700

    displaying users order in My order page done

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,8 @@ urlpatterns=[
     path("",profile,name='profile'),
     path("update/",update_user,name='update_user'),
     path("password",change_password,name='change_password'),
-    path('orders/',user_orders,name='user_orders')
+    path('orders/',user_orders,name='user_orders'),
+    path('ordered_products/',ordered_products,name='ordered_products'),
+    path('ordered_product_detail/<int:id>/<int:oid>',ordered_product_detail,name='ordered_product_detail'),
+    path('orderdetail/<int:id>/',order_detail,name='order_detail')
 ]
